468a45660ef0eca20406437b75d3950067fada1a
Thomas Jakobi <thomas.jakobi@partout.info>
2012-01-09 21:59:55 +0100

    Two new modifiers: daterange, longOptionValue Bugfix for str_replace

---
+++ daterange.phx.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * description: returns date
+ * usage: [+phx:input=`[+start+]-[+end+]`:daterange+]
+ * 
+ */
+
+$format = (strlen($options) > 0) ? $options : '%d.|%m.|%Y';
+$format = explode('|', $format);
+if (count($format) != 3) {
+	$format = explode('|', '%d.|%m.|%Y');
+}
+
+setlocale(LC_TIME, 'de_DE.UTF-8');
+$dates = explode('-', $output);
+if (!isset($dates[1])) {
+	return strftime($format[0].$format[1].$format[2], 0 + $dates[0]);
+}
+$end = strftime($format[0].$format[1].$format[2], 0 + $dates[1]);
+
+if (date('d', ($dates[1] - $dates[0])) == 1 && date('m', ($dates[1] - $dates[0])) == 1 && date('Y', ($dates[1] - $dates[0])) == 1970) {
+	$start = '';
+} elseif (date('d', ($dates[1] - $dates[0])) > 1 && date('m', ($dates[1] - $dates[0])) == 1 && date('Y', ($dates[1] - $dates[0])) == 1970) {
+	$start = strftime($format[0], 0 + $dates[0]).'–';
+} elseif (date('d', ($dates[1] - $dates[0])) > 1 && date('m', ($dates[1] - $dates[0])) > 1 && date('Y', ($dates[1] - $dates[0])) == 1970) {
+	$start = strftime($format[0].$format[1], 0 + $dates[0]).'–';
+} else {
+	$start = strftime($format[0].$format[1].$format[2], 0 + $dates[0]).'–';
+}
+return $start.$end;
+?>
--- str_replace.phx.php
+++ str_replace.phx.php
@@ -6,8 +6,8 @@
 
 if (strlen($options) > 0) {
     $data = explode("|", $options, 2);
-    $search = (!isset($data[0])) ? $data[0] : '';
-    $replace = (!isset($data[1])) ? $data[1] : '';
+    $search = (isset($data[0])) ? $data[0] : '';
+    $replace = (isset($data[1])) ? $data[1] : '';
     $result = str_replace($search, $replace, $output);
     return $result;
 }
