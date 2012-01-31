2c4cc0b433a979e4e6b383ee94da162a32967532
Thomas Jakobi <thomas.jakobi@partout.info>
2012-01-31 21:54:06 +0100

    added haschilds modifier

---
+++ haschilds.phx.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * description: condition will be set to true if the id has childs
+ * usage: [+id:haschilds:then=`…`:else=`…`+]
+ */
+
+$children = $modx->getChildIds($output, 1);
+$condition[] = (count($children) > 0) ? 1 : 0;
+?>
