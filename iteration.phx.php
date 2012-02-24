f9d668dee476946343bb1a30c0e756eb233d0705
Thomas Jakobi <thomas.jakobi@partout.info>
2012-02-24 16:51:16 +0100

    new iteration modifier

---
+++ iteration.phx.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * description: iterate counter for PHx
+ * reason:      for snippets that have not an iteration counter
+ * usage:       [+phx:iterate=`countername`+]
+ */
+
+$countername = ($options) ? 'phx_'.$options : 'phx_iterate_counter';
+
+if (isset($GLOBALS[$countername])) {
+	$GLOBALS[$countername]++;
+} else {
+	$GLOBALS[$countername] = 0;
+}
+return $GLOBALS[$countername];
+?>
