de5f26f0c7d59ba81102714fadca0796de5519ee
Thomas Jakobi <thomas.jakobi@partout.info>
2012-10-12 12:55:45 +0200

    added: cdata Modifier

--- cdata.phx.php
+++ cdata.phx.php
@@ -1,8 +1,9 @@
 <?php
- 
-// description: surround not empty string with cdata tag (closing tag has to be replaced by a plugin - otherwise it is detected as closing snippet tag)
-// usage: [+string:cdata+] 
- 
+/*
+ * description: surround not empty string with cdata tag (closing tag has to be replaced by a plugin - otherwise it is detected as closing snippet tag and will be removed by PHx)
+ * usage: [+string:cdata+] 
+ */
+
 $outer = '';
  
 if (trim($output) != '') $outer = '<![CDATA['.$output.'] ]>';
