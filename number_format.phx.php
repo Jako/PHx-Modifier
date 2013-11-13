568875bcbaccf06633a61c85c4979b6b81c96e4d
Thomas Jakobi <thomas.jakobi@partout.info>
2013-11-13 11:19:29 +0100

    bugfix

--- number_format.phx.php
+++ number_format.phx.php
@@ -5,10 +5,14 @@
  * dec_point|thousands_sep are optional
  */
 
-$options = explode('|', $options);
-if (count($options) == 3) {
-	return number_format($output, (int) $options[0], $options[1], $options[2]);
+if (!trim($output)) {
+	$options = explode('|', $options);
+	if (count($options) == 3) {
+		return number_format($output, (int) $options[0], $options[1], $options[2]);
+	} else {
+		return number_format($output, (int) $options[0]);
+	}
 } else {
-	return number_format($output, (int) $options[0]);
+	return '';
 }
 ?>
